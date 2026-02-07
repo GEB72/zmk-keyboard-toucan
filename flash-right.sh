09cb5cc2c6710d1c83e08926a26a02825fd34b27
Jakub Mikulski <jmik12131@gmail.com>
2026-02-07 01:11:53 +0000

    Config changes, increase mouse layer time

--- flash-right.sh
+++
@@ -1,5 +0,0 @@
-echo "Flashing right firmware to $1"
-mount /dev/$1 /home/jakub/mnt_r
-cp /home/jakub/Downloads/firmware/toucan_right\ rgbled_adapter-seeeduino_xiao_ble-zmk.uf2 ~/mnt
-sync
-umount /home/jakub/mnt_r
